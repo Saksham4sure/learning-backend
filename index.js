87e06c0276bd67c181c927ff39b3e163e227b2b9
Saksham4sure <sakshamorig123@gmail.com>
2026-05-30 18:43:33 +0545

    First

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const path = require('path');
+const fs = require('node:fs');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set("view engine", "ejs");
+
+app.get('/', (req, res) => {
+    fs.readdir(`./files`, (err, files) => {
+        if (err) return console.log(err);
+
+        res.render("index", { files });
+    })
+});
+
+app.post('/create', (req, res) => {
+    if(req.body.title == "" || req.body.text ==""){
+        return res.status(500).send("Cannot continue without empty name or details.")
+    }
+    fs.writeFile(`./files/${req.body.title.split(" ").join("-")}.txt`, req.body.text, (err) => {
+        res.redirect('/');
+    });
+});
+
+app.get('/file/:path', (req,res) =>{
+    fs.readFile(`./files/${req.params.path}`, "utf-8", (err, filedata) =>{
+        res.render('detail', {filename: req.params.path, filedata: filedata});
+    })
+})
+
+app.listen(3000, () => {
+    console.log("Listening at port 3000");
+});
