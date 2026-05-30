97536eb8e506e29b939fc47dbaec6f6701f8faa3
Saksham4sure <sakshamorig123@gmail.com>
2026-05-30 20:31:02 +0545

    edit

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
 });
 
 app.post('/create', (req, res) => {
-    if(req.body.title == "" || req.body.text ==""){
+    if (req.body.title == "" || req.body.text == "") {
         return res.status(500).send("Cannot continue without empty name or details.")
     }
     fs.writeFile(`./files/${req.body.title.split(" ").join("-")}.txt`, req.body.text, (err) => {
@@ -25,10 +25,26 @@ app.post('/create', (req, res) => {
     });
 });
 
-app.get('/file/:path', (req,res) =>{
-    fs.readFile(`./files/${req.params.path}`, "utf-8", (err, filedata) =>{
-        res.render('detail', {filename: req.params.path, filedata: filedata});
+app.get('/file/:path', (req, res) => {
+    fs.readFile(`./files/${req.params.path}`, "utf-8", (err, filedata) => {
+        res.render('detail', { filename: req.params.path, filedata: filedata });
     })
+})
+
+app.get('/edit/:path', (req, res) => {
+    fs.readFile(`./files/${req.params.path}`, "utf-8", (err, filedata) => {
+        res.render('edit', { filename: req.params.path, filedata: filedata });
+    })
+})
+
+app.post('/edit', (req, res) => {
+    fs.rename(`./files/${req.body.previous.split(" ").join("-")}.txt`, `./files/${req.body.new.split(" ").join("-")}.txt`, (err) => {
+        if (err) return;
+        fs.writeFile(`./files/${req.body.new.split(" ").join("-")}.txt`, req.body.newDetails, (err) => {
+            res.redirect('/')
+        })
+    })
+
 })
 
 app.listen(3000, () => {
